932c7303f5b85f3f66688a89f9f285b0cc6a282b
Jeong-DongHee <weg9511@gmail.com>
2022-02-22 02:06:28 +0900

    조회 빈이 2개 이상일 때
    1. @Autowired필드명, @Qualifier, @Primary
    1-1. 애노테이션 직접 만들기
    2. 조회한 빈이 모두 필요할 떄, List,Map

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -8,21 +9,23 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor //final 붙은 필드를 모아서 생성자로 만들어줌
+//@RequiredArgsConstructor //final 붙은 필드를 모아서 생성자로 만들어줌
 public class OrderServiceImpl implements OrderService{
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private final DiscountPolicy discountPolicy;
     private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    //    @Autowired // 생성자가 한개있으면 생략가능
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+
+    @Autowired // 생성자가 한개있으면 생략가능
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
 
